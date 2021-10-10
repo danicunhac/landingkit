465ab472906867bf01175b7f85f570f69cc42bcf
Daniel Cunha <danielcc.jp@gmail.com>
2021-10-09 21:08:43 -0300

    chore: clear template

--- src/layout/Home/index.tsx
+++ src/layout/Home/index.tsx
@@ -6,14 +6,9 @@ export const Home = () => {
   return (
     <Wrapper>
       <Head>
-        <title>Next.js Boilerplate</title>
-        <meta
-          name="description"
-          content="Next App boilerplate, typescript + eslint + prettier + editorconfig + jest + styled-components"
-        />
+        <title>Starter | Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Title text="Next.js Boilerplate w/ Common Layout Architecture" />
     </Wrapper>
   );
 };
